8887980f1f3ec964c129435c68c49f7270635a39
mfangaritav <mf.angarita52@gmail.com>
2025-11-20 15:42:09 -0900

    update Pair class

--- asf_search/Pair.py
+++ asf_search/Pair.py
@@ -1,14 +1,24 @@
 
-import fsspec
 import importlib.util
 import math
-import warnings
 
 from .ASFProduct import ASFProduct
 from .baseline import calculate_perpendicular_baselines
 from .exceptions import CoherenceEstimationError
-from .warnings import OptionalDependencyWarning
 import pytz
+
+_COHERENCE_OPT_DEPS = ['zarr', 's3fs', 'rioxarray', 'xarray']
+try:
+    for spec in _COHERENCE_OPT_DEPS:
+        if importlib.util.find_spec(spec) is None:
+            raise ImportError
+
+    import fsspec
+    import xarray as xr
+
+except ImportError:
+    fsspec = None
+    xr = None
 
 try:
     from ciso8601 import parse_datetime
@@ -20,48 +30,38 @@ class Pair:
     """
     A Pair is comprised of a reference scene and a secondary scene. These scenes typically intersect geographically,
     but that is not a requirement. When a pair is created, its perpendicular and temporal baselines are calculated
-    and stored in the self.perpendicular and self.temporal member variables.
+    and stored in the self.perpendicular_baseline and self.temporal_baseline member variables.
 
     Two pairs are equivalent if they have matching reference and secondary dates
     """
     def __init__(self, ref: ASFProduct, sec: ASFProduct):
         self.ref = ref
         self.sec = sec
+        self.id = (ref.properties['sceneName'], sec.properties['sceneName'])
 
-        self.perpendicular = calculate_perpendicular_baselines(
+        self.perpendicular_baseline = calculate_perpendicular_baselines(
             ref.properties['sceneName'], 
             [sec, ref])[0].properties['perpendicularBaseline']
 
-        ref_time = parse_datetime(ref.properties["startTime"])
-        if ref_time.tzinfo is None:
-            ref_time = pytz.utc.localize(ref_time)
-        sec_time = parse_datetime(sec.properties["startTime"])
-        if sec_time.tzinfo is None:
-            sec_time = pytz.utc.localize(sec_time)
+        self.ref_time = parse_datetime(ref.properties["startTime"])
+        if self.ref_time.tzinfo is None:
+            self.ref_time = pytz.utc.localize(self.ref_time)
+        self.sec_time = parse_datetime(sec.properties["startTime"])
+        if self.sec_time.tzinfo is None:
+            self.sec_time = pytz.utc.localize(self.sec_time)
 
-        self.ref_date = ref_time.date()
-        self.sec_date = sec_time.date()
-        self.temporal = self.sec_date - self.ref_date
-
-        # warn user if they lack optional dependency needed for estimate_s1_mean_coherence
-        if importlib.util.find_spec("xarray") is None:
-            msg = (
-                "Warning: Pair.estimate_s1_mean_coherence() requires xarray as a dependency"
-                "However, your Pair is still available without access to coherence estimation."
-            )
-            warnings.warn(OptionalDependencyWarning(msg))
+        self.temporal_baseline = self.sec_time.date() - self.ref_time.date()
 
     def __repr__(self) -> str:
-        return f"Pair({self.ref_date}, {self.sec_date})"
+        return f"Pair({self.id[0]}, {self.id[1]})"
 
     def __eq__(self, other):
         if not isinstance(other, Pair):
             return NotImplemented
-        return (self.ref_date == other.ref_date and
-                self.sec_date == other.sec_date)
+        return self.id == other.id
 
     def __hash__(self) -> int:
-        return hash((self.ref.date.date(), self.sec.date.date()))
+        return hash(self.id)
 
     def estimate_s1_mean_coherence(self) -> float:
         '''
@@ -74,7 +74,14 @@ class Pair:
 
         Returns:
         '''
-        import xarray as xr
+        if xr is None or fsspec is None:
+            raise ImportError(
+                'The `estimate_s1_mean_coherence()` method requires the optional asf-search '
+                f'dependencies {_COHERENCE_OPT_DEPS}, '
+                'but they could not be found in the current python environment. '
+                'Enable this method by including the appropriate pip or conda install. '
+                'Ex: `python -m pip install asf-search[sbas]`'
+            )
 
         month = parse_datetime(self.ref.properties["startTime"]).month
         if month in [12, 1, 2]:
@@ -86,21 +93,20 @@ class Pair:
         elif month in [9, 10, 11]:
             season = 'fall'
 
-        temporal = math.ceil(self.temporal.days / 6) * 6
+        temporal = math.ceil(self.temporal_baseline.days / 6) * 6
         if temporal > 48:
             msg = (f"""Coherence dataset includes temporal baselines up to 48 days.
-                   Temporal baseline: {self.temporal.days} days""")
+                   Temporal baseline: {self.temporal_baseline.days} days""")
             raise CoherenceEstimationError(msg)
 
-        temporal = str(temporal).zfill(2)
-        uri = f"s3://asf-search-coh/global_coh_100ppd_11367x4367/Global_{season}_vv_COH{temporal}_100ppd.zarr"
+        uri = f"s3://asf-search-coh/global_coh_100ppd_11367x4367_Zarrv2/Global_{season}_vv_COH{temporal}_100ppd.zarr"
         coords = self.ref.geometry['coordinates'][0]
         lons, lats = zip(*coords)
         minx, miny, maxx, maxy = min(lons), min(lats), max(lons), max(lats)
 
         ds = xr.open_zarr(
             fsspec.get_mapper(uri, s3={'anon': True}),
-            consolidated=True,
+            consolidated=False
             )
         ds = ds.rio.write_crs("EPSG:4326", inplace=False)
         subset = ds.rio.clip_box(minx=minx, miny=miny, maxx=maxx, maxy=maxy)
